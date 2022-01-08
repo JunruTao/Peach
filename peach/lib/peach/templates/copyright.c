dbe3f590cd00cb792d48457cf63b19ecec52196d
Junru_aliendeveloper <junrutao@qq.com>
2022-01-08 21:05:37 +0000

    adding license

---
+++ peach/lib/peach/templates/copyright.c
@@ -0,0 +1,4 @@
+// * Copyright (C) Digital Peach Studio - All Rights Reserved
+// * Unauthorized copying of this file, via any medium is strictly prohibited
+// * Proprietary and confidential
+// * Written by Junru Tao <junrutao.uk@gmail.com>, Jan 2022
