bea3078835c9a32dca94abb4a3daf3dcf4e582a8
Junru_aliendeveloper <junrutao@qq.com>
2022-01-12 10:18:58 +0000

    adding cmake cmd to sh

--- peach/build_install.sh
+++ peach/build_install.sh
@@ -3,4 +3,5 @@
 # Make and Install
 
 cd ./.build
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
 eval "mingw32-make install"
--- peach/clean_debug_install.sh
+++ peach/clean_debug_install.sh
@@ -3,4 +3,8 @@
 powershell "./dev_osTools/clean.ps1"
 
 cd ./.build
+
+# cmake debug
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
+# make install
 eval "mingw32-make install"
