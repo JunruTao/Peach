4af26ab29f736f661fd66c9c65f1929a9c67b616
Junru_aliendeveloper <junrutao@qq.com>
2022-01-20 22:37:40 +0000

    fixing build install libonly sript

--- peach/dev_osTools/cmake_configlive/cmake_config_devlib_only.py
+++ peach/dev_osTools/cmake_configlive/cmake_config_devlib_only.py
@@ -1,6 +1,23 @@
 import os
 
+
+main_cmake_str = ""
+with open("./CMakeLists.txt", "r") as f:
+    for line in f:
+        if str(line).startswith("set(BUILD_DEV_NOT_INSTALL"):
+            line = "set(BUILD_DEV_NOT_INSTALL TRUE)  # >>>> CONTROL [AUTO]\n"
+            print("[settings-cmake]: ", line.rstrip())
+        if str(line).startswith("set(BUILD_DEVLIB_ONLY"):
+            line = "set(BUILD_DEVLIB_ONLY TRUE)      # >>>> CONTROL [AUTO]\n"
+            print("[settings-cmake]: ", line.rstrip())
+        main_cmake_str += line
+    f.close()
     
+
+with open("./CMakeLists.txt", "w+") as f:
+    f.write(main_cmake_str)
+    f.close()
+
 main_cmake_str = ""
 with open("./lib/CMakeLists.txt", "r") as f:
     for line in f:
